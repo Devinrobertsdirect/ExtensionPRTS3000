45a99f70e03e3083fd87c37aee6d0c0c19c31529
Devin_Roberts <DCRoberts1@student.fullsail.edu>
2024-04-02 18:33:05 -0400

    A2

--- ExtensionPRTS/Level1.js
+++ ExtensionPRTS/Level1.js
@@ -1,7 +1,15 @@
-document.getElementById('startCallBtn').addEventListener('click', () => {
+let timerInterval;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000);
+    // Set call status to "started" in Chrome storage
     chrome.storage.local.set({ callData: { status: 'started', level: 1 } });
-});
+}
+document.getElementById('startCallBtn').addEventListener('click', startTimer);
 document.getElementById('toLevel2Btn').addEventListener('click', () => {
             // Redirect to Level 2 interface
-            window.location.href = 'Level2.html';
-        });
+    window.location.href = 'Level2.html';
+});
